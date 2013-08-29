c6dec6a8a7b0c3c28fa10756a1a73d3fdecaa63a
spurioso <spurioso@gmail.com>
2013-08-28 22:19:53 -0400

    generated permutations of meeting days and printed the combinations as text

--- meetingSchedule.py
+++ meetingSchedule.py
@@ -1,3 +1,5 @@
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 sunday = 0
 monday = 1
 tuesday = 2
@@ -6,10 +8,32 @@ thursday = 4
 friday = 5
 saturday = 6
 
-schedule = [monday, tuesday, thursday, friday]
-
 shortestSpan = 15
+longestSpan = 0
 
+def all_perms(days):
+    if len(days) <=1:
+        yield days
+    else:
+        for perm in all_perms(days[1:]):
+            for i in range(len(days)):
+                #nb schedule[0:1] works in both string and list contexts
+                yield perm[:i] + days[0:1] + perm[i:]
+
+days = [monday, tuesday, thursday, friday]
+
+
+schedule = all_perms(days)
+for i in schedule:
+    print (i)
+    for j in range(len(i)):
+        #print j
+        #print i[j]
+        dayCode = i[j]
+        print week[dayCode]
+
+
+'''
 for j in range(0,4):
     thisWeek = schedule[j]
     nextWeek = schedule[(j+1) % 4]
@@ -19,5 +43,5 @@ for j in range(0,4):
         shortestSpan = timeBetween
 
 print "The shortest time between meetings is " + str(shortestSpan) + " days"
-
+'''
 
