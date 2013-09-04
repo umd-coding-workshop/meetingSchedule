b3ef0f17408c87e3632555e135f418d1cee9fc66
Ben Wallberg <wallberg@umd.edu>
2013-09-04 19:36:54 -0400

    GH-1. add unit tests
    also update code to Python 3.3 so we can use the latest version of
    the unittest module
    
    TODO: separate running unit tests from running the program
    
    https://github.com/umd-coding-workshop/meetingSchedule/issues/1

--- meetingSchedule.py
+++ meetingSchedule.py
@@ -1,3 +1,5 @@
+import unittest
+
 week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 sunday = 0
@@ -19,27 +21,51 @@ def all_perms(days):
                 #nb schedule[0:1] works in both string and list contexts
                 yield perm[:i] + days[0:1] + perm[i:]
 
-days = [monday, tuesday, thursday, friday]
-
-# see http://stackoverflow.com/questions/231767/the-python-yield-keyword-explained for figuring out how this works.
-schedule = all_perms(days)
-option = 1
-for i in schedule:
-    print "Option " + str(option) + ":"
+def analyze_sequence(seq):
+    """Analyze a sequence of days in a week and return the shortest span between days."""
+    
     shortestSpan = 15
     longestSpan = 0
-    for j in range(len(i)):
+    for j in range(len(seq)):
         #print j
         #print i[j]
-        dayCode = i[j]
-        print "Week " + str(j+1) + ": " + str(week[dayCode])
+        dayCode = seq[j]
+        print("Week " + str(j+1) + ": " + str(week[dayCode]))
 
-    for j in range(len(i)):
-        thisWeek = i[j]
-        nextWeek = i[(j+1) % 4]
+    for j in range(len(seq)):
+        thisWeek = seq[j]
+        nextWeek = seq[(j+1) % 4]
         timeBetween = nextWeek + 7 - thisWeek
-        print "There are %s days between week %s and week %s meetings" % (timeBetween, j+1, j+2)
+        print("There are %s days between week %s and week %s meetings" % (timeBetween, j+1, j+2))
         if timeBetween < shortestSpan:
             shortestSpan = timeBetween
-    print "The shortest time between meetings is " + str(shortestSpan) + " days"    
-    option = option + 1
+
+    return shortestSpan
+    
+
+def main():
+    days = [monday, tuesday, thursday, friday]
+
+    # see http://stackoverflow.com/questions/231767/the-python-yield-keyword-explained for figuring out how this works.
+    schedule = all_perms(days)
+    option = 1
+    for i in schedule:
+        print("Option " + str(option) + ":")
+        shortestSpan = analyze_sequence(i)
+        print("The shortest time between meetings is " + str(shortestSpan) + " days")
+        option = option + 1
+    
+class MeetingScheduleTestCase(unittest.TestCase):
+    """Unit tests for meetingSchedule."""
+    
+    def testAnalyzeSequence(self):
+        self.assertEqual(analyze_sequence([monday,tuesday,thursday,friday]),  3)
+        self.assertEqual(analyze_sequence([tuesday,thursday,monday,friday]),  4)
+        self.assertEqual(analyze_sequence([thursday,tuesday,monday,friday]),  5)
+        
+if __name__ == '__main__':
+    test = unittest.main(exit=False)
+    
+    if (test.result.wasSuccessful()):
+        main()
+
